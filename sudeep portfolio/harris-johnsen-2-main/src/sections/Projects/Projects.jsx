74f9207fda24faf081ebb4c86babfad4f6b636ac
Your Name <your.email@example.com>
2025-02-05 15:25:49 +0530

    netflix

--- sudeep portfolio/harris-johnsen-2-main/src/sections/Projects/Projects.jsx
+++ sudeep portfolio/harris-johnsen-2-main/src/sections/Projects/Projects.jsx
@@ -1,9 +1,11 @@
 import styles from './ProjectsStyles.module.css';
-import beaton from '../../assets/beat-on.png'
-import freshBurger from '../../assets/fresh-burger.png';
-import hipsster from '../../assets/hipsster.png';
-import fitLift from '../../assets/fitlift.png';
+import beaton from '../../assets/beat-on.png';
+import whatsApp from '../../assets/whatsApp.png';
+import netflix from '../../assets/netflix logo.png';
+import family from '../../assets/family.png';
+import weather from '../../assets/weather.png';
 import ProjectCard from '../../common/ProjectCard';
+import pdf from '../../assets/Sudeeps Project report.pdf';
 
 function Projects() {
   return (
@@ -17,22 +19,28 @@ function Projects() {
           p="Music Streaming App"
         />
         <ProjectCard
-          src={freshBurger}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Fresh Burger"
-          p="Hamburger Restaurant"
+          src={whatsApp}
+          link={pdf}
+          h3="Webhook"
+          p="WhatsApp Chatbot"
         />
         <ProjectCard
-          src={hipsster}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Hipsster"
-          p="Glasses Shop"
+          src={family}
+          link="https://github.com/Sudeep-07/WD/tree/main/back%20end%20development/postgreSQL/8.5%2BFamily%2BTravel%2BTracker/8.5%20Family%20Travel%20Tracker"
+          h3="WanderNest"
+          p="Family Travel Tracker"
         />
         <ProjectCard
-          src={fitLift}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="FitLift"
-          p="Fitness App"
+          src={netflix}
+          link="https://github.com/Sudeep-07/projects/tree/main/NETFLIX"
+          h3="Netflix"
+          p="Netflix Clone"
+        />
+        <ProjectCard
+          src={weather}
+          link="https://github.com/Sudeep-07/projects/tree/main/weatherApp"
+          h3="SkyCast"
+          p="Weather Forecast"
         />
       </div>
     </section>
